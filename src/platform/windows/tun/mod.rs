bae6b8affe688e03008ca74e5c6a57006675cd1b
xmh0511 <970252187@qq.com>
2025-03-17 13:12:24 +0800

    cargo clippy

--- src/platform/windows/tun/mod.rs
+++ src/platform/windows/tun/mod.rs
@@ -1,4 +1,4 @@
-use std::os::windows::io::{AsRawHandle, FromRawHandle, OwnedHandle};
+use std::os::windows::io::{AsRawHandle, OwnedHandle};
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::{io, ptr};
 
