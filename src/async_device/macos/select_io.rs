237319df669ea100f18f3a30ee313429ad01b4f1
lbl <1791778603@qq.com>
2025-05-30 15:44:06 +0800

    Enhance the readable and writable event handling for macOS feth.

--- src/async_device/macos/select_io.rs
+++ src/async_device/macos/select_io.rs
@@ -5,22 +5,20 @@ use std::io::{IoSlice, IoSliceMut};
 use std::ops::Deref;
 use std::os::fd::AsRawFd;
 use std::pin::Pin;
-use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::{Arc, Mutex};
 use std::task::{Context, Poll};
+use std::time::Duration;
 
 pub(crate) struct NonBlockingDevice {
     device: DeviceImpl,
-    is_shutdown: AtomicBool,
-    event_fd: EventFd,
+    shutdown_event: EventFd,
 }
 impl NonBlockingDevice {
     pub(crate) fn new_dev(device: DeviceImpl) -> io::Result<Self> {
         device.set_nonblocking(true)?;
         Ok(Self {
             device,
-            is_shutdown: Default::default(),
-            event_fd: EventFd::new()?,
+            shutdown_event: EventFd::new()?,
         })
     }
 
@@ -39,54 +37,70 @@ impl NonBlockingDevice {
     }
 
     pub(crate) fn shutdown(&self) -> io::Result<()> {
-        self.is_shutdown.store(true, Ordering::Relaxed);
-        self.event_fd.wake()
+        self.shutdown_event.wake()
     }
 }
 
 impl NonBlockingDevice {
-    pub fn wait_write(&self) -> io::Result<()> {
+    pub fn wait_writable(&self, cancel_event: Option<libc::c_int>) -> io::Result<()> {
         let readfds: libc::fd_set = unsafe { std::mem::zeroed() };
         let mut writefds: libc::fd_set = unsafe { std::mem::zeroed() };
         let fd = self.device.as_raw_fd();
         unsafe {
             libc::FD_SET(fd, &mut writefds);
         }
-        self.wait(readfds, writefds)
+        self.wait(readfds, writefds, cancel_event)
     }
-    pub fn wait_read(&self) -> io::Result<()> {
+    pub fn wait_readable(&self, cancel_event: Option<libc::c_int>) -> io::Result<()> {
         let mut readfds: libc::fd_set = unsafe { std::mem::zeroed() };
         let writefds: libc::fd_set = unsafe { std::mem::zeroed() };
         let fd = self.device.as_raw_fd();
         unsafe {
             libc::FD_SET(fd, &mut readfds);
         }
-        self.wait(readfds, writefds)
+        self.wait(readfds, writefds, cancel_event)
     }
-    fn wait(&self, mut readfds: libc::fd_set, mut writefds: libc::fd_set) -> io::Result<()> {
+    fn wait(
+        &self,
+        mut readfds: libc::fd_set,
+        mut writefds: libc::fd_set,
+        cancel_event: Option<libc::c_int>,
+    ) -> io::Result<()> {
         let fd = self.device.as_raw_fd();
-        let event_fd = self.event_fd.as_event_fd();
+        let event_fd = self.shutdown_event.as_event_fd();
         let mut errorfds: libc::fd_set = unsafe { std::mem::zeroed() };
+        let mut nfds = fd.max(event_fd);
         unsafe {
             libc::FD_SET(event_fd, &mut readfds);
+            if let Some(cancel_event) = cancel_event {
+                libc::FD_SET(cancel_event, &mut readfds);
+                nfds = nfds.max(cancel_event);
+            }
         }
         let result = unsafe {
             libc::select(
-                fd.max(event_fd) + 1,
+                nfds + 1,
                 &mut readfds,
                 &mut writefds,
                 &mut errorfds,
                 std::ptr::null_mut(),
             )
         };
-        if self.is_shutdown.load(Ordering::Relaxed) {
-            return Err(io::Error::new(io::ErrorKind::ConnectionAborted, "close"));
-        }
         if result < 0 {
             return Err(io::Error::last_os_error());
         }
         if result == 0 {
             return Err(io::Error::from(io::ErrorKind::TimedOut));
+        }
+        unsafe {
+            if libc::FD_ISSET(event_fd, &readfds) {
+                return Err(io::Error::new(io::ErrorKind::UnexpectedEof, "close"));
+            }
+            if let Some(cancel_event) = cancel_event {
+                if libc::FD_ISSET(cancel_event, &readfds) {
+                    return Err(io::Error::new(io::ErrorKind::Interrupted, "cancel"));
+                }
+            }
         }
         Ok(())
     }
@@ -111,6 +125,33 @@ impl EventFd {
         } else {
             Ok(())
         }
+    }
+    fn wait_timeout(&self, timeout: Duration) -> io::Result<()> {
+        let mut readfds = unsafe {
+            let mut set = std::mem::zeroed::<libc::fd_set>();
+            libc::FD_ZERO(&mut set);
+            libc::FD_SET(self.0, &mut set);
+            set
+        };
+        let mut tv = libc::timeval {
+            tv_sec: timeout.as_secs() as libc::time_t,
+            tv_usec: timeout.subsec_micros() as libc::suseconds_t,
+        };
+        let res = unsafe {
+            libc::select(
+                self.0 + 1,
+                &mut readfds,
+                std::ptr::null_mut(),
+                std::ptr::null_mut(),
+                &mut tv,
+            )
+        };
+        if res < 0 {
+            return Err(io::Error::last_os_error());
+        } else if res == 0 {
+            return Err(io::Error::from(io::ErrorKind::TimedOut));
+        }
+        Ok(())
     }
     fn as_event_fd(&self) -> libc::c_int {
         self.0
@@ -155,12 +196,26 @@ impl AsyncDevice {
 impl AsyncDevice {
     pub async fn readable(&self) -> io::Result<()> {
         let device = self.inner.clone();
-        blocking::unblock(move || device.wait_read()).await?;
+        let mut canceller = Canceller::new_cancelable()?;
+        let (cancel_guard, exit_guard) = canceller.guard(device);
+        blocking::unblock(move || {
+            exit_guard
+                .call(|device, cancel_event| device.wait_readable(Some(cancel_event.as_event_fd())))
+        })
+        .await?;
+        std::mem::forget(cancel_guard);
         Ok(())
     }
     pub async fn writable(&self) -> io::Result<()> {
         let device = self.inner.clone();
-        blocking::unblock(move || device.wait_write()).await?;
+        let mut canceller = Canceller::new_cancelable()?;
+        let (cancel_guard, exit_guard) = canceller.guard(device);
+        blocking::unblock(move || {
+            exit_guard
+                .call(|device, cancel_event| device.wait_writable(Some(cancel_event.as_event_fd())))
+        })
+        .await?;
+        std::mem::forget(cancel_guard);
         Ok(())
     }
     pub fn poll_readable(&self, cx: &mut Context<'_>) -> Poll<io::Result<()>> {
@@ -169,7 +224,7 @@ impl AsyncDevice {
             task
         } else {
             let device = self.inner.clone();
-            blocking::unblock(move || device.wait_read())
+            blocking::unblock(move || device.wait_readable(None))
         };
         match Pin::new(&mut task).poll(cx) {
             Poll::Ready(rs) => {
@@ -205,7 +260,7 @@ impl AsyncDevice {
             task
         } else {
             let device = self.inner.clone();
-            blocking::unblock(move || device.wait_write())
+            blocking::unblock(move || device.wait_writable(None))
         };
         match Pin::new(&mut task).poll(cx) {
             Poll::Ready(rs) => match rs {
@@ -279,5 +334,75 @@ impl AsyncDevice {
     }
     pub fn try_send_vectored(&self, bufs: &[IoSlice<'_>]) -> io::Result<usize> {
         self.inner.try_send_vectored(bufs)
+    }
+}
+
+struct ExitSignalGuard {
+    device: Option<Arc<NonBlockingDevice>>,
+    cancel_event_handle: Arc<EventFd>,
+    exit_event: Arc<EventFd>,
+}
+impl Drop for ExitSignalGuard {
+    fn drop(&mut self) {
+        drop(self.device.take());
+        _ = self.exit_event.wake();
+    }
+}
+impl ExitSignalGuard {
+    pub fn call<R>(
+        &self,
+        mut op: impl FnMut(&NonBlockingDevice, &EventFd) -> io::Result<R>,
+    ) -> io::Result<R> {
+        if let Some(device) = &self.device {
+            op(device, &self.cancel_event_handle)
+        } else {
+            unreachable!()
+        }
+    }
+}
+
+struct Canceller {
+    exit_event_handle: Arc<EventFd>,
+    cancel_event_handle: Arc<EventFd>,
+}
+
+impl Canceller {
+    fn new_cancelable() -> io::Result<Self> {
+        Ok(Self {
+            exit_event_handle: Arc::new(EventFd::new()?),
+            cancel_event_handle: Arc::new(EventFd::new()?),
+        })
+    }
+
+    fn guard(
+        &mut self,
+        device_impl: Arc<NonBlockingDevice>,
+    ) -> (CancelWaitGuard<'_>, ExitSignalGuard) {
+        (
+            CancelWaitGuard {
+                exit_event_handle: &self.exit_event_handle,
+                cancel_event_handle: &self.cancel_event_handle,
+            },
+            ExitSignalGuard {
+                device: Some(device_impl),
+                exit_event: self.exit_event_handle.clone(),
+                cancel_event_handle: self.cancel_event_handle.clone(),
+            },
+        )
+    }
+}
+
+struct CancelWaitGuard<'a> {
+    exit_event_handle: &'a Arc<EventFd>,
+    cancel_event_handle: &'a Arc<EventFd>,
+}
+
+impl Drop for CancelWaitGuard<'_> {
+    fn drop(&mut self) {
+        if self.cancel_event_handle.wake().is_ok() {
+            _ = self
+                .exit_event_handle
+                .wait_timeout(Duration::from_millis(1))
+        }
     }
 }
