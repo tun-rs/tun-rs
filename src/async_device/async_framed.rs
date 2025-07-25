3bf9e45106fe1d774b3ff1f76e92963d5cfd9e71
xmh0511 <970252187@qq.com>
2025-07-25 09:53:30 +0800

    cargo fmt

--- src/async_device/async_framed.rs
+++ src/async_device/async_framed.rs
@@ -303,7 +303,6 @@ impl<C, T> DeviceFramedRead<C, T>
 where
     T: Borrow<AsyncDevice>,
 {
-
     /// Construct from a [`AsyncDevice`] with a specific codec.
     ///
     /// The read side of the framed device.
@@ -409,7 +408,6 @@ impl<C, T> DeviceFramedWrite<C, T>
 where
     T: Borrow<AsyncDevice>,
 {
-  
     /// Construct from a [`AsyncDevice`] with a specific codec.
     ///
     /// The write side of the framed device.
