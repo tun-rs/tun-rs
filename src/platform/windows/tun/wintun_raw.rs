64ffab5875f617ace0f7cf320c1a33176b2ac786
xmh0511 <970252187@qq.com>
2025-08-18 20:40:25 +0800

    refine names

--- src/platform/windows/tun/wintun_raw.rs
+++ src/platform/windows/tun/wintun_raw.rs
@@ -1,12 +1,12 @@
 #![allow(warnings)]
-#[cfg(not(docsrs))]
-#[cfg(feature = "bindgen")]
+
+#[cfg(all(not(docsrs), feature = "bindgen"))]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 #[cfg(any(docsrs, not(feature = "bindgen")))]
 #[cfg(target_pointer_width = "64")]
-include!("bindings.rs");
+include!("bindings_x86_64.rs");
 
 #[cfg(any(docsrs, not(feature = "bindgen")))]
 #[cfg(target_pointer_width = "32")]
-include!("bindings_x86.rs");
+include!("bindings_i686.rs");
